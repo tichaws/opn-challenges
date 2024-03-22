70508407dcdfa564e1b9a6bbf49b37925746c8fb
laphatrada <tichaws@gmail.com>
2024-03-22 16:14:35 +1100

    refactor the code to be more readable and enhance reusability.

--- challenge-react/src/App.js
+++ challenge-react/src/App.js
@@ -1,9 +1,20 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import fetch from 'isomorphic-fetch';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { summaryDonations } from './helpers';
 import './styles.css';
+
+const FlexContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`
+
+const Flex = styled.div`
+  display: flex;
+  justify-content: center;
+}`;
 
 const Container = styled.div`
   display: flex;
@@ -24,180 +35,264 @@ const Card = styled.div`
   overflow: hidden;
 }`;
 
-const Popup = styled.div`
-    position: absolute;
-    display:flex;
-    color: rgb(255, 255, 255);
-    height:100%;
-    width: 100%;
-    background: white;
-    opacity: 93%;
+const CardDisplay = styled.div`
+  border-radius: 32px;
+  width: 25rem;
 }`;
 
-const Flex = styled.div`
+const CardImage = styled.div`
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-top-left-radius: 0.25rem;
+  border-top-right-radius: 0.25rem;
+  filter: contrast(100%);
+  overflow: hidden;
+  position: relative;
+  height: 10em;
+  width: 28rem;
+  transition: filter 0.5s cubic-bezier(.43, .41, .22, .91);
+}`;
+
+
+const CardContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+}`;
+
+const CardBtn = styled.div`
+  background-color: white;
+  border: 1px solid blue;
+  color: blue;
+  padding: 0.5rem;
+  cursor: pointer;
+  display: flex;
+  justify-content: flex-end;
+`
+
+const Popup = styled.div`
+  position: absolute;
+  display:flex;
+  color: rgb(255, 255, 255);
+  height:100%;
+  width: 100%;
+  background: white;
+  opacity: 93%;
+}`;
+
+const PopContainer = styled.div`
   display: flex;
   justify-content: center;
+  flex-direction: column;
+  margin: auto;
+  color: black;
+  grid-gap: 1rem;
 }`;
 
+const PopCloseBtn = styled.div`
+  position: absolute;
+  float: right;
+  color: grey;
+  background: transparent;
+  border: 0;
+  top: 0;
+  right: 0;
+  padding: 1rem;
+  cursor: pointer;
 
+  &:hover {
+    color: black;
+  }
+}`;
 
-export default connect((state) => state)(
-  class App extends Component {
-    state = {
-      charities: [],
-      selectedAmount: 10,
-      selectedCharityId: null
-    };
+const Btn = styled.div`
+  background-color: white;
+  border: 1px solid blue;
+  color: blue;
+  padding: 0.5rem;
+  cursor: pointer;
+`
 
-    componentDidMount() {
-      const self = this;
-      fetch('http://localhost:3001/charities')
-        .then(function (resp) {
-          return resp.json();
-        })
-        .then(function (data) {
-          self.setState({ charities: data });
-        });
+const Message = styled.div`
+  color: white;
+  margin: 1em 0px;
+  font-weight: bold;
+  font-size: 16px;
+  text-align: center;
+  background: cadetblue;
+  padding: 6px;
+  border-radius: 10px;
+`
 
-      fetch('http://localhost:3001/payments')
-        .then(function (resp) {
-          return resp.json();
-        })
-        .then(function (data) {
-          self.props.dispatch({
-            type: 'UPDATE_TOTAL_DONATE',
-            amount: summaryDonations(data.map((item) => item.amount)),
-          });
-        });
-    }
+const App = (props) => {
+  const [selectedAmount, setSelectedAmount] = React.useState(0);
+  const [charities, setCharities] = React.useState([]);
+  const [selectedCharityId, setSelectedCharityId] = React.useState(0);
+  const [paymentAmount] = React.useState([10, 20, 50, 100, 500]);
+  
+  const geturl = (path) => {
+    return `http://localhost:3001/${path}`
+  };
 
-    handleMessage(msg) {
-      const self = this;
-      self.props.dispatch({
-        type: 'UPDATE_MESSAGE',
-        message: msg,
+  const handleMessage = (msg) => {
+    props.dispatch({
+      type: 'UPDATE_MESSAGE',
+      message: msg,
+    });
+  }
+
+  const handleTotalAmount = (amount) => {
+    props.dispatch({
+      type: 'UPDATE_TOTAL_DONATE',
+      amount,
+    });
+  }
+
+  const fetchCharities = () => {
+    fetch(geturl('charities'))
+      .then((res) => res.json())
+      .then((data) => {
+        setCharities(data);
       });
-    }
+  };
 
-    handleAmount = (amount) => {
-      const self = this;
-      this.setState({
-        selectedAmount: amount
+  const fetchPayments = () => {
+    fetch(geturl('payments'))
+      .then((res) => res.json())
+      .then((data) => {
+        handleTotalAmount(summaryDonations(data.map((item) => item.amount)));
       });
-      self.handleMessage('')
+  };
 
-    };
+  const handleAmount = (amount) => {
+    setSelectedAmount(amount);
+    handleMessage('')
+  };
 
-    handleSelected = (id) => {
-      const self = this;
-      self.handleMessage('')
-      this.setState({
-        selectedCharityId: id
-      });
-    };
+  const handleSelected = (id) => {
+    handleMessage('')
+    setSelectedCharityId(id);
+  };
 
-    handlePay = (id, amount, currency) => {
-      const self = this;
-      fetch('http://localhost:3001/payments', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ 'charitiesId': id, 'amount': amount, 'currency': currency })
-      }).then(function (resp) {
-        return resp.json();
-      }).then((data) => {
-        self.props.dispatch({
-          type: 'UPDATE_TOTAL_DONATE',
-          amount: data.amount,
-        })
-        self.handleMessage('Thank you for your donate')
-      });
-    }
+  useEffect(() => {
+    console.log(props);
+    fetchCharities();
+    fetchPayments();
+  }, [])
 
-    validateAmount = (item) => {
-      const self = this;
-      if (self.state.selectedAmount > 0) {
-        self.handlePay.call(
-          self,
-          item.id,
-          self.state.selectedAmount,
-          item.currency
-        )
-        self.handleAmount(null)
-      }
-    }
+  const handlePay = (id, amount, currency) => {
+    fetch(geturl('payments'), {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ charitiesId: id, amount, currency}),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      handleTotalAmount(data.amount);
+      handleMessage('Thank you for your donate')
+    });
+  }
 
-    render() {
-      const self = this;
-      const cards = this.state.charities.map(function (item, i) {
-        const payments = [10, 20, 50, 100, 500].map((amount, j) => (
-          <label key={j}>
-            <input
-              type="radio"
-              checked={self.state.selectedAmount===amount}
-              name={`payment_${item.id}`}
-              onChange={function () {
-                self.handleAmount(amount);
-              }}
-            />
-            {amount}
-          </label>
-        ));
-        return (
-          <Card key={i}>
-            <div className="card__display">
-              <div className="card__image">
-                <img src={`./images/${item.image}`} />
-              </div>
-              <div className="card__content">
-                <div className="card__title">{item.name}</div>
-                <button className="btn card__btn"
-                  onClick={function () {
-                    self.handleSelected(item.id);
-                  }}
-                >Donation
-                </button>
-              </div>
-            </div>
-            <Popup key={i} style={{ display: self.state.selectedCharityId === item.id ? 'flex' : 'none' }}>
-              <div className="pop_container">
-                <div className="close"
-                  onClick={function () {
-                    self.handleSelected(null);
-                  }}>
-                  X
-                </div>
-                <span>Select the amount to donation(USD)</span>
-                <Flex>{payments}</Flex>
-                <Flex>
-                  <button
-                    className="btn"
-                    onClick={() => self.validateAmount(item)}>
-                    Pay
-                  </button>
-                </Flex>
-              </div>
-            </Popup>
-          </Card>
-        );
-      });
-
-      const donate = this.props.donate;
-      const message = this.props.message;
-
-      return (
-        <Flex style={{ 'flexDirection': 'column' }}>
-          <h1 style={{ 'margin': 'auto' }}>Tamboon React</h1>
-          <p>All donations: {donate} USD</p>
-          <div className='message' style={{ display: self.props.message ? 'block' : 'none' }}>
-            {message}
-          </div>
-          <Container>
-            {cards}
-          </Container>
-        </Flex >
-      );
+  const validateAmount = (item) => {
+    if (selectedAmount > 0) {
+      handlePay.call(
+        selectedCharityId,
+        item.id,
+        selectedAmount,
+        item.currency
+      )
+      handleAmount(null)
     }
   }
-);
+
+  const Tiele = ({ text }) => {
+    return <h1 style={{ margin: 'auto' }}>{text}</h1>;
+  };
+
+  const radioSelect = (onselect, charitiesId) => {
+    return paymentAmount.map((amount, j) => (
+      <label key={j}>
+        <input
+          type="radio"
+          checked={selectedAmount === amount}
+          name={`payment_${charitiesId}`}
+          onChange={() => onselect(amount)}
+        />
+        {amount}
+      </label>
+    ));
+  }
+
+  const selectMode = (item, onclose, onpay, payments) => {
+    return <Popup style={{ display: selectedCharityId === item.id ? 'flex' : 'none' }}>
+      <PopContainer>
+        <PopCloseBtn
+          onClick={() => onclose()}>
+          X
+        </PopCloseBtn>
+        <span>Select the amount to donation(USD)</span>
+        <Flex>{payments}</Flex>
+        <Flex>
+          <Btn
+            className="btn"
+            onClick={() => onpay(item)}>
+            Pay
+          </Btn>
+        </Flex>
+      </PopContainer>
+    </Popup>;
+  }
+
+  const cards = charities.map(function (item, i) {
+
+    const payments = radioSelect((amount) => handleAmount(amount), item.id)
+    const pop = selectMode(item, () => handleSelected(null), (item) => validateAmount(item), payments)
+
+    return (
+      <Card key={i}>
+        <CardDisplay >
+          <CardImage >
+            <img src={`./images/${item.image}`} />
+          </CardImage>
+          <CardContent >
+            <div className="card__title">{item.name}</div>
+            <CardBtn
+              onClick={function () {
+                handleSelected(item.id);
+              }}
+            >Donation
+            </CardBtn>
+          </CardContent>
+        </CardDisplay>
+        {pop}
+      </Card>
+    );
+  });
+
+  return (
+    <div>
+      <FlexContainer>
+        <Tiele text={'Tamboon React'} />
+        <p>All donations: {props.donate} </p>
+        <Message style={{ display: props.message ? 'block' : 'none' }}>
+          {props.message}
+        </Message>
+        <Container>
+          {cards}
+        </Container>
+      </FlexContainer >
+    </div>
+  );
+};
+
+const mapStateToProps = function (state) {
+  return {
+    donate: state.donate,
+    message: state.message,
+  }
+};
+
+export default connect(mapStateToProps)(App);
